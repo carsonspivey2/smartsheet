2ebe7e4f9887189dca807ec7b7744bef2911f9d3
Gabriel Dibble <gdibble@gmail.com>
2023-06-22 12:12:20 -0700

    Add fourth test sheet id to config and its get script Config updated with 4th test sheet id plus workspace-id and sheet name for reference.  Adds get_sheet_test4.py.

---
+++ get_sheet_test4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import configparser
+import smartsheet
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Read API key from config file
+api_key = config.get('Smartsheet', 'API_KEY')
+
+# Initialize Smartsheet client
+client = smartsheet.Smartsheet(api_key)
+
+# Read Smartsheet ID from config file
+smartsheet_id = config.get('Smartsheet', 'SOURCE_SMARTSHEET_TEST4_ID')
+
+# Get Template
+sheet = client.Sheets.get_sheet(smartsheet_id)
+print(sheet)
